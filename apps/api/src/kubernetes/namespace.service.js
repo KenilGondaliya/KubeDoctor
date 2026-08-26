d2523869a5e49b2d15a02ece9dca3bae0ee0ab97
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2026-08-26 10:47:16 +0530

    commit

--- apps/api/src/kubernetes/namespace.service.js
+++
@@ -1,9 +0,0 @@
-import { coreApi } from "./client.js";
-
-export async function getNamespaces() {
-  const response = await coreApi.listNamespace();
-  return response.items.map((n) => ({
-    name: n.metadata?.name,
-    status: n.status?.phase,
-  }));
-}
--- node_modules/kubedoctor-api/src/kubernetes/client.js
+++
@@ -1,9 +0,0 @@
-import * as k8s from '@kubernetes/client-node';
-
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
-
-const coreApi = kc.makeApiClient(k8s.CoreV1Api);
-const appsApi = kc.makeApiClient(k8s.AppsV1Api);
-
-export { kc, coreApi, appsApi };
