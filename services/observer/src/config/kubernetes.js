1d6582403a9470d43acea54d4037283ef79f3830
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2026-08-26 20:44:38 +0530

    commit

---
+++ services/observer/src/config/kubernetes.js
@@ -0,0 +1,25 @@
+import * as k8s from "@kubernetes/client-node";
+
+export function loadKubeConfig(contextName) {
+  const config = new k8s.KubeConfig();
+
+  config.loadFromDefault();
+
+  if (contextName) {
+    config.setCurrentContext(contextName);
+  }
+
+  return config;
+}
+
+export function createCoreApi(contextName) {
+  const config = loadKubeConfig(contextName);
+
+  return config.makeApiClient(k8s.CoreV1Api);
+}
+
+export function createAppsApi(contextName) {
+  const config = loadKubeConfig(contextName);
+
+  return config.makeApiClient(k8s.AppsV1Api);
+}
